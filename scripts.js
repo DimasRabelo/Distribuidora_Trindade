cef5a8381be4be68430baf07da182c62fcf72f1e
Dimas <dimas_ap_souza@yahoo.com.br>
2025-01-12 14:06:22 -0300

    update

--- scripts.js
+++ scripts.js
@@ -1,16 +1,24 @@
+let imagem = document.querySelector(".imagem-ovo"); 
+let circulo = document.querySelector(".circulo");
+let textos = document.querySelectorAll(".descricao p"); // Todos os elementos <p>
 
-
-
-let imagem = document.querySelector(".imagem-ovo") 
-let circulo = document.querySelector(".circulo")
-
-
-function trocarImagem(endereco){
-
-   
- imagem.src = endereco
+function trocarImagem(endereco) {
+    imagem.src = endereco;
 }
 
-function trocarCor(cor){
-    circulo.style.background = cor
+function trocarCor(cor) {
+    circulo.style.background = cor;
+}
+
+function mostrarTexto(textoId) {
+    // Primeiro, esconde todos os textos
+    textos.forEach(texto => {
+        texto.style.display = 'none';
+    });
+
+    // Mostra o texto correspondente
+    let textoSelecionado = document.getElementById(textoId);
+    if (textoSelecionado) {
+        textoSelecionado.style.display = 'block';
+    }
 }
